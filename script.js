05ddc31d6751a867bd338e5b790e1df94bb4fb8b
Ryan Lirio <ryanlirio2@gmail.com>
2025-02-04 16:47:16 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// Script para alternar o menu em telas menores
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
